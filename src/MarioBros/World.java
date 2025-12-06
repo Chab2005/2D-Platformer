ddfa8761061c6b2f55a4062ac07b3a18218e70a6
Chab <palexandrechabot@gmail.com>
2025-12-06 14:44:04 -0500

    refactor : divide into methods tile generation

--- src/MarioBros/World.java
+++ src/MarioBros/World.java
@@ -5,7 +5,6 @@ import Doctrina.Canvas;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
@@ -16,38 +15,38 @@ public class World extends StaticEntity {
     private static String JSON_FILE_PATH = "resources/json/TEMP4BG.json";
     private static String MAP_PATH = "images/tilesheet/tilesheet.png";
     private Color backgroundColor;
-    private Image background;
+
+    private BackGround backGround;
     private JsonNode jsonObj;
     private ObjectMapper objectMapper;
     private CollidableRepository collidableRepository;
-    private Renderer renderer;
+    //private Renderer renderer;
     private Layer layers;
     private BlockTypeService blockTypeService;
+    private BufferedImage spriteSheet;
 
 
     public World() {
         blockTypeService = new BlockTypeService();
-
+        backGround = new BackGround();
         layers = new Layer();
         jsonParser = new JsonParser();
         objectMapper = new ObjectMapper();
         collidableRepository = collidableRepository.getInstance();
-        x = 0;
-        y = 0;
     }
 
     public void load() {
-
         loadMap();
         setBackgroundColor();
     }
 
     public void draw(Canvas canvas) {
-        //canvas.drawRectangle(0,0,Integer.MAX_VALUE,Integer.MAX_VALUE, Color.CYAN);
-        //canvas.drawImage(background,x,y);
+
+
     }
 
     private void loadMap() {
+        spriteSheet = loadSpriteSheet();
         initMap();
         setMap();
         loadEntities();
@@ -58,44 +57,52 @@ public class World extends StaticEntity {
     }
 
     private void initEntites(){
-        SpriteLoader spriteLoader = new SpriteLoader();
-        BufferedImage spriteSheet = spriteLoader.loadSpriteSheet(MAP_PATH);
-
         for (Chunks chunk : layers.getChunks()) {
             int arrayChunk[] =  chunk.getData();
-            int rowWidth = chunk.getWidth(); // 30
-            int colHeight = chunk.getHeight(); // 300
-            System.out.println(rowWidth + " " + colHeight);
-
-            int x = 0;
-            int y = 0;
-            for (int i = 0; i < arrayChunk.length; i++) {
-                x++;
-                if(i%300 == 0) {
-                    x = 0;
-                    y++;
-                }
-
-                if (!blockTypeService.isAir(arrayChunk[i])) {
-                    System.out.println(arrayChunk[i]);
-                    Brick brick = new Brick(
-                            getRightBlockPosition(x),
-                            getRightBlockPosition(y),
-                            getBrickSprite(spriteSheet,arrayChunk[i])
-                    );
-
-                    if (!blockTypeService.isDecoration(arrayChunk[i])) {
-                        collidableRepository.registerEntity(brick);
-                    }
-
-                    RenderingRepository.getInstance().registerEntities(brick);
-                }
-            }
-
-
+            initChunk(chunk, arrayChunk);
         }
     }
 
+    private void initChunk(Chunks chunk, int[] arrayChunk) {
+        int positionX = 0;
+        int positionY = 0;
+        for (int i = 0; i < arrayChunk.length; i++) {
+            positionX++;
+            if(isNextLineChunk(i, chunk.getHeight())) {
+                positionX = 0;
+                positionY++;
+            }
+
+            if (!blockTypeService.isAir(arrayChunk[i])) {
+                initEntity(arrayChunk[i], positionX, positionY);
+            }
+        }
+    }
+
+
+    private boolean isNextLineChunk(int index,int widthLine) {
+        return index % widthLine == 0;
+    }
+
+    private BufferedImage loadSpriteSheet() {
+        SpriteLoader spriteLoader = new SpriteLoader();
+        return spriteLoader.loadSpriteSheet(MAP_PATH);
+    }
+
+    private void initEntity(int index, int positionX, int positionY) {
+
+        Brick brick = new Brick(
+                getRightBlockPosition(positionX),
+                getRightBlockPosition(positionY),
+                getBrickSprite(spriteSheet,index)
+        );
+
+        if (!blockTypeService.isDecoration(index)) {
+            collidableRepository.registerEntity(brick);
+        }
+
+        RenderingRepository.getInstance().registerEntities(brick);
+    }
 
     private int getRightBlockPosition(int position) {
         return Math.abs(position * 16);
@@ -103,9 +110,7 @@ public class World extends StaticEntity {
 
     private Image getBrickSprite(BufferedImage spriteSheet,int index){
         int positionX = blockTypeService.getPosX(index);
-
         int positionY = blockTypeService.getPosY(index);
-
         return spriteSheet.getSubimage(positionX,positionY,16,16);
     }
 
@@ -118,7 +123,7 @@ public class World extends StaticEntity {
     }
 
     private void setMap() {
-        JsonNode layerArrayNode =  jsonObj.get("layers");
+        JsonNode layerArrayNode = jsonObj.get("layers");
         try {
             for (JsonNode layerNode : layerArrayNode) {
                 Chunks chunk = objectMapper.treeToValue(layerNode, Chunks.class);
